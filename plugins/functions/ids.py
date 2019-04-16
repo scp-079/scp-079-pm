c746dbe34cbc6f0491af816f12034ce49d18ea0d
Xiao <xiao@scp-079.org>
2019-04-16 14:00:33 +0000

    0.2.0

--- plugins/functions/ids.py
+++ plugins/functions/ids.py
@@ -1,3 +1,21 @@
+# SCP-079-PM - Everyone can have their own Telegram private chat bot
+# Copyright (C) 2019 SCP-079 <https://scp-079.org>
+#
+# This file is part of SCP-079-PM.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import logging
 
 from .. import glovar
