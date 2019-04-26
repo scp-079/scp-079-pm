6ff1455185be61036686b28fb405672858660e2a
Xiao <xiao@scp-079.org>
2019-04-26 00:59:20 +0000

    0.2.2

--- plugins/functions/deliver.py
+++ plugins/functions/deliver.py
@@ -17,9 +17,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+from functools import partial
 from time import sleep
 
-from pyrogram import Client, InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram import Client, InlineKeyboardButton, InlineKeyboardMarkup, Message, ParseMode
 from pyrogram.errors import FloodWait, UserIsBlocked
 
 from .. import glovar
@@ -29,6 +30,120 @@ from .telegram import send_message
 
 # Enable logging
 logger = logging.getLogger(__name__)
+
+
+def forward(
+        self,
+        chat_id: int or str,
+        disable_notification: bool = None,
+        as_copy: bool = False,
+        remove_caption: bool = False,
+        reply_to_message_id: int = None
+) -> "Message":
+    if as_copy:
+        if self.service:
+            raise ValueError("Unable to copy service messages")
+
+        if self.game and not self._client.is_bot:
+            raise ValueError("Users cannot send messages with Game media type")
+
+        if self.text:
+            return self._client.send_message(
+                chat_id,
+                text=self.text.html,
+                parse_mode="html",
+                disable_web_page_preview=not self.web_page,
+                disable_notification=disable_notification,
+                reply_to_message_id=reply_to_message_id
+            )
+        elif self.media:
+            caption = self.caption.html if self.caption and not remove_caption else None
+
+            send_media = partial(
+                self._client.send_cached_media,
+                chat_id=chat_id,
+                disable_notification=disable_notification,
+                reply_to_message_id=reply_to_message_id
+            )
+
+            if self.photo:
+                file_id = self.photo.sizes[-1].file_id
+            elif self.audio:
+                file_id = self.audio.file_id
+            elif self.document:
+                file_id = self.document.file_id
+            elif self.video:
+                file_id = self.video.file_id
+            elif self.animation:
+                file_id = self.animation.file_id
+            elif self.voice:
+                file_id = self.voice.file_id
+            elif self.sticker:
+                file_id = self.sticker.file_id
+            elif self.video_note:
+                file_id = self.video_note.file_id
+            elif self.contact:
+                return self._client.send_contact(
+                    chat_id,
+                    phone_number=self.contact.phone_number,
+                    first_name=self.contact.first_name,
+                    last_name=self.contact.last_name,
+                    vcard=self.contact.vcard,
+                    disable_notification=disable_notification,
+                    reply_to_message_id=reply_to_message_id
+                )
+            elif self.location:
+                return self._client.send_location(
+                    chat_id,
+                    latitude=self.location.latitude,
+                    longitude=self.location.longitude,
+                    disable_notification=disable_notification,
+                    reply_to_message_id=reply_to_message_id
+                )
+            elif self.venue:
+                return self._client.send_venue(
+                    chat_id,
+                    latitude=self.venue.location.latitude,
+                    longitude=self.venue.location.longitude,
+                    title=self.venue.title,
+                    address=self.venue.address,
+                    foursquare_id=self.venue.foursquare_id,
+                    foursquare_type=self.venue.foursquare_type,
+                    disable_notification=disable_notification,
+                    reply_to_message_id=reply_to_message_id
+                )
+            elif self.poll:
+                return self._client.send_poll(
+                    chat_id,
+                    question=self.poll.question,
+                    options=[opt.text for opt in self.poll.options],
+                    disable_notification=disable_notification,
+                    reply_to_message_id=reply_to_message_id
+                )
+            elif self.game:
+                return self._client.send_game(
+                    chat_id,
+                    game_short_name=self.game.short_name,
+                    disable_notification=disable_notification,
+                    reply_to_message_id=reply_to_message_id
+                )
+            else:
+                raise ValueError("Unknown media type")
+
+            if self.sticker or self.video_note:  # Sticker and VideoNote should have no caption
+                return send_media(file_id=file_id)
+            else:
+                return send_media(file_id=file_id, caption=caption, parse_mode=ParseMode.HTML)
+        else:
+            raise ValueError("Can't copy this message")
+    else:
+        return self._client.forward_messages(
+            chat_id=chat_id,
+            from_chat_id=self.chat.id,
+            message_ids=self.message_id,
+            disable_notification=disable_notification,
+            reply_to_message_id=reply_to_message_id
+        )
 
 
 def deliver_guest_message(client: Client, message: Message) -> bool:
@@ -42,6 +157,11 @@ def deliver_guest_message(client: Client, message: Message) -> bool:
         else:
             as_copy = True
 
+        reply_mid = None
+        if message.reply_to_message:
+            reply_mid = message.reply_to_message.message_id
+            reply_mid = glovar.reply_ids["guest"].get(reply_mid, (None, 0))[0]
+
         result = None
         while not result:
             try:
@@ -51,6 +171,12 @@ def deliver_guest_message(client: Client, message: Message) -> bool:
                     message_ids=mids,
                     disable_notification=True,
                     as_copy=as_copy
+                )
+                result = forward(
+                    self=message,
+                    chat_id=hid,
+                    as_copy=as_copy,
+                    reply_to_message_id=reply_mid
                 )
             except FloodWait as e:
                 sleep(e.x + 1)
