abf604fd28ab3ade72db2b42d7b8903174144b94
Xiao <xiao@scp-079.org>
2019-06-17 12:47:11 +0000

    0.3.9

--- plugins/handlers/message.py
+++ plugins/handlers/message.py
@@ -51,7 +51,6 @@ def count(client: Client, message: Message):
                    & ~Filters.command(glovar.all_commands, glovar.prefix))
 def deliver_to_guest(client: Client, message: Message):
     try:
-        logger.warning(message)
         hid = message.chat.id
         mid = message.message_id
         _, cid = get_guest(message)
