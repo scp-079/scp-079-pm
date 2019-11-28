447ce648503ca9c8603418253ae830565f8b6a7e
Xiao <xiao@scp-079.org>
2019-11-28 14:18:31 +0000

    - 0.5.0

--- plugins/glovar.py
+++ plugins/glovar.py
@@ -191,13 +191,13 @@ lang: Dict[str, str] = {
     "description_choose_clear": (zh_cn and "请选择要清空的数据") or "Please Choose the Data to Clear",
     "description_choose_recall": (zh_cn and "请选择要撤回全部消息的类别") or "Please Choose the Messages to Recall",
     "description_direct": ((zh_cn and ("如需将消息转发给某人，"
-                                       "请以 /direct 命令回复某条包含该用户 ID 的汇报消息，并转发消息给机器人。"
+                                       "请以 /direct 命令回复某条包含该用户 ID 的汇报消息，并转发消息到当前对话中。"
                                        "注意：此时将开启与该用户的直接对话，您发送给机器人的任何消息都将发送给对方，"
                                        "而无需回复带该用户 ID 的汇报消息。"
                                        "如欲退出与该用户的直接对话，请发送：/leave 指令")
                             or ("To forward a message to someone, "
                                 "reply with a report message containing the user ID with the /direct command "
-                                "and forward the message to the robot. "
+                                "and forward the message to the current chat. "
                                 "Note: A direct conversation with the user will be initiated "
                                 "and any messages you send to the bot will be sent to the user "
                                 "without having to reply to the report message with that user ID. "
